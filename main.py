6f15540dadd462cf5ad0fca2d967951ffae2c41f
Anmol <anmol.nit2020@gmail.com>
2025-02-03 21:00:20 +0530

    code refactored

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from utils import *
 
 def read_pdf(pdf_path : str):
     records = []
-    months = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
     
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
