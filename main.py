b82dab9abd95002439693a2e1505e1dee70bb2db
Anmol <anmol.nit2020@gmail.com>
2025-02-11 18:07:47 +0530

    small changes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pdfplumber
 import pandas as pd
 from utils import *
+from tqdm import tqdm
 
 def read_pdf(pdf_path : str):
     records = []
     
     with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
+        for page in tqdm(pdf.pages,desc='Reading PDF'):
             text = page.extract_text()
             lines = text.split("\n")
  
@@ -35,19 +36,19 @@ def process_trasaction_text(tail):
 
 def convert_to_df(tail):
     data = {
-    "Date" : [],
-    "Paid to" : [],
-    "Type" : [],
-    "Amount" : [],
-    "Time" : [],
-    "Transaction ID" : [],
-    "UTR" : [],
-    "Account Number" : []
+        "Date" : [],
+        "Paid to" : [],
+        "Type" : [],
+        "Amount" : [],
+        "Time" : [],
+        "Transaction ID" : [],
+        "UTR" : [],
+        "Account Number" : []
     }
 
     transactions = process_trasaction_text(tail)
 
-    for t in transactions:
+    for t in tqdm(transactions,desc='Processing'):
         if len(t) == 4:
             td = extract_transaction_details(t[0])
             tm = extract_time_and_transaction_id(t[1])
@@ -68,8 +69,9 @@ def convert_to_df(tail):
 
 
 if __name__ == '__main__':
-    pdf_path = "Input/PhonePe_Statement_Jan2025_Feb2025.pdf"
-    output_path = "Output/PhonePe_Statement_Jan2025_Feb2025.xlsx"
+    input_file = 'PhonePe_Statement_Feb2024_Feb2025'
+    pdf_path = f"Input/{input_file}.pdf"
+    output_path = f"Output/{input_file}.xlsx"
     head, tail = read_pdf(pdf_path)
     df = convert_to_df(tail)
     df.to_excel(output_path, index=False)
